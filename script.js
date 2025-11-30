7465adef7f8b1f69c4fafefb3ed057fb063dca5d
deveshgoyal2070-pixel <deveshgoyal.2070@gmail.com>
2025-11-30 10:23:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const welcomeBox = document.getElementById("welcomeBox");
-
-function selectTool(id) {
-  if (welcomeBox) welcomeBox.classList.add("hidden");
-}
-
-function applyFilter() {}
-function resetImageFilters() {}
-function downloadImage() {}
